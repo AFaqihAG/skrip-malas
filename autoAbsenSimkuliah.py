60092124366bb60050d9db1ec51a0b19a81c75bc
AFaqihAG <anulerreborn@gmail.com>
2023-10-05 12:58:49 +0700

    Code Optimisation

--- autoAbsenSimkuliah.py
+++ autoAbsenSimkuliah.py
@@ -9,77 +9,9 @@ import requests
 import time
 
 # Akun USK untuk Login
-NIM = "NIM"
-PASS = "PASS"
+NIM = "2108107010072"
+PASS = "95684712"
 MAX_WAIT_TIME = 20 # waktu tunggu maksimal dalam seconds
-
-# Fungsi untuk mengklik elemen by CSS SELECTOR setelah menunggu
-def click_by_css(css_selector, label):
-    try:
-        WebDriverWait(driver, MAX_WAIT_TIME).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector))
-        ).click()
-        print(f"Tombol '{label}' di klik")
-    except TimeoutException:
-        print(f"Tombol '{label}' tidak ditemukan atau tidak dapat diklik.")
-        exit_program("Terjadi kesalahan!")
-
-# Fungsi untuk mengisi teks pada kolom berdasarkan atribut name dan nilainya 
-def fill_text_by_name(value_of_name, text_to_fill):
-    try:
-        elemen = WebDriverWait(driver, MAX_WAIT_TIME).until(
-            EC.presence_of_element_located((By.NAME, value_of_name))
-        )
-        elemen.clear()
-        elemen.send_keys(text_to_fill)
-    except TimeoutException:
-        print(f"Elemen dengan atribut 'NAME'='{value_of_name}' tidak ditemukan atau tidak dapat diisi.")
-        exit_program("Terjadi kesalahan!")
-
-# Fungsi untuk mencetak teks dari elemen dengan selector CSS
-def print_text_by_css(css_selector, message=""):
-    try:
-        elemen = WebDriverWait(driver, MAX_WAIT_TIME).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
-        )
-        teks_elemen = elemen.text
-        print(f"{message}{teks_elemen}")
-    except TimeoutException:
-        print(f"'{css_selector}' tidak ditemukan.")
-        exit_program("Terjadi kesalahan!")
-
-# Fungsi untuk mencetak teks dari elemen dengan selector CSS
-def get_text_by_css(css_selector):
-    try:
-        elemen = WebDriverWait(driver, MAX_WAIT_TIME).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
-        )
-        text = elemen.text
-        return text
-    except TimeoutException:
-        return None
-
-# Fungsi untuk membuka atau pindah url baru
-def go_to_url(url):
-    # opening the target website in the browser 
-    driver.get(url)
-    #printing the target website url and title
-    print(f"Membuka '{driver.current_url}' ")
-
-# Fungsi untuk keluar program
-def exit_program(message):
-    print(message, "Exiting..")
-    driver.close()
-    exit()
-
-# Fungsi untuk memeriksa koneksi internet
-def is_internet_available(url="https://google.com"):
-    try:
-        # Cobalah menghubungi server Google
-        response = requests.get(url, timeout=10000)
-        return True
-    except requests.ConnectionError:
-        return False
 
 # the target website 
 URL_LOGIN = "https://simkuliah.usk.ac.id/index.php/login" 
@@ -101,76 +33,130 @@ VALID_ALERT_SELECTOR = "body > section > div > div > div > div.login-card.card-b
 MK_SEKARANG_SELECTOR = "#pcoded > div.pcoded-container.navbar-wrapper > div > div > div.pcoded-content > div > div > div > div.page-body > div > div > div > div.card-header > h5"
 INFO_ABSENSI_SELECTOR = "#pcoded > div.pcoded-container.navbar-wrapper > div > div > div.pcoded-content > div > div > div > div.page-body > div > div > div > div.card-block > div > div:nth-child(1) > div > p"
 
-if is_internet_available():
-    # using Firefox headless webdriver to secure connection to Firefox 
-    # with webdriver.Firefox(options=options) as driver:
+def click_by_css(css_selector, label):
+    try:
+        WebDriverWait(driver, MAX_WAIT_TIME).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector))
+        ).click()
+        print(f"Tombol '{label}' di klik")
+    except TimeoutException:
+        print(f"Tombol '{label}' tidak ditemukan atau tidak dapat diklik.")
+        exit_program("Terjadi kesalahan!")
 
-    go_to_url(URL_LOGIN)  # pergi ke halaman login
-    # Mengisi username, password and signin elements
-    fill_text_by_name("username", NIM)
-    fill_text_by_name("password", PASS)
+def fill_text_by_name(value_of_name, text_to_fill):
+    try:
+        elemen = WebDriverWait(driver, MAX_WAIT_TIME).until(
+            EC.presence_of_element_located((By.NAME, value_of_name))
+        )
+        elemen.clear()
+        elemen.send_keys(text_to_fill)
+    except TimeoutException:
+        print(f"Elemen dengan atribut 'NAME'='{value_of_name}' tidak ditemukan atau tidak dapat diisi.")
+        exit_program("Terjadi kesalahan!")
+
+def print_text_by_css(css_selector, message=""):
+    try:
+        elemen = WebDriverWait(driver, MAX_WAIT_TIME).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
+        )
+        teks_elemen = elemen.text
+        print(f"{message}{teks_elemen}")
+    except TimeoutException:
+        print(f"'{css_selector}' tidak ditemukan.")
+        exit_program("Terjadi kesalahan!")
+
+def get_text_by_css(css_selector):
+    try:
+        elemen = WebDriverWait(driver, MAX_WAIT_TIME).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
+        )
+        text = elemen.text
+        return text
+    except TimeoutException:
+        return None
+
+def go_to_url(url): 
+    driver.get(url)
+    print(f"Membuka '{driver.current_url}' ")
+
+def exit_program(message):
+    print(message, "Exiting..")
+    driver.close()
+    exit()
+
+def is_internet_available(url="https://google.com"):
+    try:
+        response = requests.get(url, timeout=10000)
+        return True
+    except requests.ConnectionError:
+        return False
     
-    # Flow click tombol
-    # Flow Login
-    click_by_css(LOGIN_SELECTOR, "Login") 
-    # Jika berhasil masuk
-    nama = get_text_by_css(NAMA_AKUN_SELECTOR)
-    if nama:
-        length = 6 + len(nama)
-        print("Login Berhasil!")
-        print('='* math.ceil((length-6)/2) ,"INFO", '='* math.ceil((length-6)/2) )
-        print("Nama:",nama)
-        print("NIM:",NIM)
-        print('='*(length+1))
-        
-        # Flow pergi ke halaman absensi 
-        go_to_url(URL_ABSENSI)  # pergi ke halaman absensi
-        check = get_text_by_css(CHECK_ABSEN_SELECTOR)    # Mendapatkan teks jika absen belum tersedia
-        
-        # Jika absen belum tersedia maka program berhenti
-        if "sudah" in check.lower():
-            exit_program(check)    
-        else:
-            mataKuliah = get_text_by_css(MK_SEKARANG_SELECTOR)     # Dapatkan mata kuliah yang sedang berlangsung
-            info_absensi_check = get_text_by_css(INFO_ABSENSI_SELECTOR)  # dapatkan teks anda sudah absen atau belum
+def print_login_info(nama, NIM):
+    length = 6 + len(nama)
+    print("Login Berhasil!")
+    print('=' * math.ceil((length - 6) / 2), "INFO", '=' * math.ceil((length - 6) / 2))
+    print("Nama:", nama)
+    print("NIM:", NIM)
+    print('=' * (length + 1))
 
-            if mataKuliah and info_absensi_check:
-                print("Mata Kuliah:", mataKuliah)
-
-                # dapatkan kata kedua yaitu belum atau sudah dari info_absensi_check
-                info_c = info_absensi_check.split()
-                if (info_c[1].lower() == "belum"):
-                    print(info_absensi_check)
-                    # click_by_css(konfirmasi_kehadiran, "konfirmasi kehadiran")
-                    # click_by_css(konfirmasi, "konfirmasi absen")
-                    time.sleep(3.0)
-                    driver.find_element(By.CSS_SELECTOR, KONFIRMASI_KEHADIRAN_SELECTOR).click()
-                    time.sleep(3.0)
-                    driver.find_element(By.CSS_SELECTOR, KONFIRMASI_SELECTOR).click()
-
-                    go_to_url(URL_ABSENSI)  # pergi ke halaman absensi
-                    info_absensi_check = get_text_by_css(INFO_ABSENSI_SELECTOR)  # dapatkan teks anda sudah absen atau belum
-                    info_c = info_absensi_check.split()
-                    if (info_c[1].lower() == "sudah"):
-                        exit_program(f"Absensi {mataKuliah}\nberhasil!")
-                    else:
-                        exit_program(info_absensi_check)
-                # jika selain belum
-                else:
-                    exit_program(info_absensi_check)
-
-            else:
-                exit_program("Gagal mengindeks MK dan Info_absen")
-
+def check_sudah_absensi(info_check, mataKuliah): 
+    if (info_check == "sudah"):
+        exit_program(f"Absensi {mataKuliah} berhasil!")
     else:
-        # Menampilkan kesalahan jika username atau pass salah
-        if NIM and PASS:    
-            error_alert = get_text_by_css(VALID_ALERT_SELECTOR)
-            exit_program(error_alert)
-        else:
-            exit_program("NIM/Password kosong!")
+        exit_program(f"Absensi {mataKuliah} tidak berhasil")
 
-        exit_program("Gagal login!")
-else:
-    gagal = "Gagal Terhubung. Coba lagi nanti."
-    exit_program(gagal)
+def login_and_get_nama(username, password):
+    go_to_url(URL_LOGIN)
+    fill_text_by_name("username", username)
+    fill_text_by_name("password", password)
+    click_by_css(LOGIN_SELECTOR, "Login")
+    nama = get_text_by_css(NAMA_AKUN_SELECTOR)
+    return nama
+
+def handle_absensi():
+    go_to_url(URL_ABSENSI)
+    check_absen = get_text_by_css(CHECK_ABSEN_SELECTOR)
+    
+    if "sudah" in check_absen.lower():
+        exit_program(check_absen)    
+    else:
+        mata_kuliah = get_text_by_css(MK_SEKARANG_SELECTOR)
+        info_absensi_check = get_text_by_css(INFO_ABSENSI_SELECTOR)
+
+        if mata_kuliah:
+            print("Mata Kuliah:", mata_kuliah)
+
+            info_check = info_absensi_check.split()
+            if info_check[1].lower() == "belum":
+                print(info_absensi_check)
+                confirm_attendance(mata_kuliah)
+            else:
+                exit_program(info_absensi_check)
+        else:
+            exit_program(info_absensi_check)
+
+def confirm_attendance(mata_kuliah):
+    time.sleep(3.0)
+    driver.find_element(By.CSS_SELECTOR, KONFIRMASI_KEHADIRAN_SELECTOR).click()
+    time.sleep(3.0)
+    driver.find_element(By.CSS_SELECTOR, KONFIRMASI_SELECTOR).click()
+
+    go_to_url(URL_ABSENSI)
+    info_absensi_check = get_text_by_css(INFO_ABSENSI_SELECTOR)
+    info_check = info_absensi_check.split()
+
+    check_sudah_absensi(info_check[1].lower(), mata_kuliah)
+
+def main():
+    if is_internet_available():
+        nama = login_and_get_nama(NIM, PASS)
+        if nama is not None:
+            print_login_info(nama, NIM)
+            handle_absensi()
+        else:
+            exit_program("Gagal login!, Periksa NIM/Password!")
+    else:
+        gagal = "Gagal Terhubung. Coba lagi nanti."
+        exit_program(gagal)
+
+main()
